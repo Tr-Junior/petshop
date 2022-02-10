011492e3de1055c40df91588a35cfd0cd0b965c6
Tarcísio Júnior <83793209+osicrat@users.noreply.github.com>
2022-02-10 16:49:55 -0300

    pet-shop primeira versão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule, ToastNoAnimation, ToastNoAnimationModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FramePageComponent } from './pages/master/frame.page';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { MaskDirective } from './directives/mask.directive';
+import { DataService } from './services/data.service';
+import { AuthService } from './services/auth.service';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    MaskDirective,
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    ProductsPageComponent,
+    PetsPageComponent,
+    CartPageComponent,
+    FramePageComponent,
+    ProductCardComponent,
+    LoadingComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    //BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [DataService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
